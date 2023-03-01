55c9e5ca9dc2174ab7e11cb1e7d3c4075d103495
Tarmo Asikainen <tare.asikainen@gmail.com>
2023-03-01 19:45:53 +0200

    4.4

--- round4/bloglist/src/utils/list_helper.js
+++ round4/bloglist/src/utils/list_helper.js
@@ -1,5 +1,8 @@
 const dummy = () => 1;
 
+const totalLikes = (blogs) => blogs.reduce((total, currentBlog) => total + currentBlog.likes, 0);
+
 module.exports = {
   dummy,
+  totalLikes,
 };
--- round4/bloglist/tests/list_helper.test.js
+++ round4/bloglist/tests/list_helper.test.js
@@ -1,8 +1,59 @@
 const listHelper = require('../src/utils/list_helper');
 
-test('dummy returns one', () => {
-  const blogs = [];
+describe('dummy', () => {
+  test('should return one', () => {
+    const blogs = [];
 
-  const result = listHelper.dummy(blogs);
-  expect(result).toBe(1);
+    const result = listHelper.dummy(blogs);
+    expect(result).toBe(1);
+  });
+});
+
+describe('totalLikes', () => {
+  const oneBlog = [{
+    _id: '1',
+    title: 'test',
+    author: 'test',
+    url: 'test',
+    likes: 1,
+  }];
+
+  const threeBlogs = [
+    {
+      _id: '1',
+      title: 'test',
+      author: 'test',
+      url: 'test',
+      likes: 1,
+    },
+    {
+      _id: '2',
+      title: 'test2',
+      author: 'test',
+      url: 'test',
+      likes: 3,
+    },
+    {
+      _id: '3',
+      title: 'test3',
+      author: 'test',
+      url: 'test',
+      likes: 3,
+    },
+  ];
+  test('should return zero for empty list', () => {
+    const blogs = [];
+
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(0);
+  });
+  test('should return correct value for one blog', () => {
+    const result = listHelper.totalLikes(oneBlog);
+    expect(result).toBe(oneBlog[0].likes);
+  });
+  test('should return correct value for multiple blogs', () => {
+    const totalCount = 7;
+    const result = listHelper.totalLikes(threeBlogs);
+    expect(result).toBe(totalCount);
+  });
 });
